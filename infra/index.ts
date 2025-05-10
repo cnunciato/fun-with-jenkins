ea360db44e710c46065b11f23498612c7f013ed3
Christian Nunciato <chris@nunciato.org>
2025-05-10 07:49:55 -0700

    Use EC2 instead

--- infra/index.ts
+++ infra/index.ts
@@ -1,56 +1,89 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as fs from "fs";
 
+// Get some configuration values or set default values.
 const config = new pulumi.Config();
-const containerPort = config.getNumber("containerPort") || 80;
-const cpu = config.getNumber("cpu") || 512;
-const memory = config.getNumber("memory") || 128;
-const jenkinsAdminPassword = config.requireSecret("jenkinsAdminPassword");
+const instanceType = config.get("instanceType") || "t3.micro";
+const vpcNetworkCidr = config.get("vpcNetworkCidr") || "10.0.0.0/16";
 
-// An ECS cluster to deploy into
-const cluster = new aws.ecs.Cluster("cluster", {});
+// Look up the latest Amazon Linux 2 AMI.
+const ami = aws.ec2.getAmi({
+    filters: [{
+        name: "name",
+        values: ["amzn2-ami-hvm-*"],
+    }],
+    owners: ["amazon"],
+    mostRecent: true,
+}).then(invoke => invoke.id);
 
-// An ALB to serve the container endpoint to the internet
-const loadbalancer = new awsx.lb.ApplicationLoadBalancer("loadbalancer", {});
+// User data to start a HTTP server in the EC2 instance
+const userData = fs.readFileSync("userdata.sh", "utf-8");
 
-// An ECR repository to store our application's container image
-const repo = new awsx.ecr.Repository("repo", {
-    forceDelete: true,
+// Create VPC.
+const vpc = new aws.ec2.Vpc("vpc", {
+    cidrBlock: vpcNetworkCidr,
+    enableDnsHostnames: true,
+    enableDnsSupport: true,
 });
 
-// Build and publish our application's container image from ./app to the ECR repository
-const image = new awsx.ecr.Image("image", {
-    repositoryUrl: repo.url,
-    context: "./app",
-    platform: "linux/amd64",
+// Create an internet gateway.
+const gateway = new aws.ec2.InternetGateway("gateway", {vpcId: vpc.id});
+
+// Create a subnet that automatically assigns new instances a public IP address.
+const subnet = new aws.ec2.Subnet("subnet", {
+    vpcId: vpc.id,
+    cidrBlock: "10.0.1.0/24",
+    mapPublicIpOnLaunch: true,
 });
 
-// Deploy an ECS Service on Fargate to host the application container
-const service = new awsx.ecs.FargateService("service", {
-    cluster: cluster.arn,
-    assignPublicIp: true,
-    taskDefinitionArgs: {
-        container: {
-            name: "app",
-            image: image.imageUri,
-            cpu: cpu,
-            memory: memory,
-            essential: true,
-            portMappings: [{
-                containerPort: containerPort,
-                hostPort: containerPort,
-                targetGroup: loadbalancer.defaultTargetGroup,
-            }],
-            environment: [
-                {
-                    name: "JENKINS_ADMIN_PASSWORD",
-                    value: jenkinsAdminPassword,
-                },
-            ],
-        },
+// Create a route table.
+const routeTable = new aws.ec2.RouteTable("routeTable", {
+    vpcId: vpc.id,
+    routes: [{
+        cidrBlock: "0.0.0.0/0",
+        gatewayId: gateway.id,
+    }],
+});
+
+// Associate the route table with the public subnet.
+const routeTableAssociation = new aws.ec2.RouteTableAssociation("routeTableAssociation", {
+    subnetId: subnet.id,
+    routeTableId: routeTable.id,
+});
+
+// Create a security group allowing inbound access over port 80 and outbound
+// access to anywhere.
+const secGroup = new aws.ec2.SecurityGroup("secGroup", {
+    description: "Enable HTTP access",
+    vpcId: vpc.id,
+    ingress: [{
+        fromPort: 8080,
+        toPort: 8080,
+        protocol: "tcp",
+        cidrBlocks: ["0.0.0.0/0"],
+    }],
+    egress: [{
+        fromPort: 0,
+        toPort: 0,
+        protocol: "-1",
+        cidrBlocks: ["0.0.0.0/0"],
+    }],
+});
+
+// Create and launch an EC2 instance into the public subnet.
+const server = new aws.ec2.Instance("server", {
+    instanceType: instanceType,
+    subnetId: subnet.id,
+    vpcSecurityGroupIds: [secGroup.id],
+    userData: userData,
+    ami: ami,
+    tags: {
+        Name: "webserver",
     },
 });
 
-// The URL at which the container's HTTP endpoint will be available
-export const url = pulumi.interpolate`http://${loadbalancer.loadBalancer.dnsName}`;
+// Export the instance's publicly accessible IP address and hostname.
+export const ip = server.publicIp;
+export const hostname = server.publicDns;
+export const url = pulumi.interpolate`http://${server.publicDns}`;
